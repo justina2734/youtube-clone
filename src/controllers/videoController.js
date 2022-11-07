be3c9c7e06d521654ef44e9ff56763837419d2d1
justina2734 <justina2734@gmail.com>
2022-11-08 07:27:35 +0900

    #6.0~#6.6
    array database & edit video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,15 @@
-const fakeUser = {
+/*const fakeUser = {
     username: "Yelim",
     loggedIn: false,
-};
+};  pageTitle에 fakeUser:fakeUser 추가
+*/
 let videos = [
     {
      title:"First Video",
      rating:5,
      comments:10,
      createdAt:"10 minutes ago",
-     views:78,
+     views:1,
      id:1,
     },
     {
@@ -29,13 +30,29 @@ let videos = [
     }
 ];
 export const trending = (req, res) => {
-    res.render("home", { pageTitle: "Home", videos, fakeUser: fakeUser })
+    res.render("home", { pageTitle: "Home", videos});
 };
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.render("home");
-export const upload = (req, res) => res.render("watch");
-export const deleteVideo = (req, res) => res.render("edit");
+export const watch = (req, res) => {
+    const id = req.params.id;
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle:`Watching: ${video.title}`, video});
+};
+export const getEdit = (req, res) => {
+    const id = req.params.id;
+    const video = videos[id-1];
+    return res.render("edit", {pageTitle:`Editing: ${video.title}`, video})
+};
+export const postEdit = (req, res) => {
+    const id = req.params.id;
+    const title = req.body.title;
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
+};
 
-
-
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle:"Upload Video"});
+}
+export const postUpload = (req, res) => {
+    console.log(req.body);
+    return res.redirect("/");
+};
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {join, login} from "../controllers/userController";/*default export 를 안쓰면 이런식으로 import 함*/
-import {trending, search} from "../controllers/videoController";/*../는 지금 있는 폴더에서 벗어난다는 의미, ./는 지금의 장소를 의미*/
+import {trending} from "../controllers/videoController";/*../는 지금 있는 폴더에서 벗어난다는 의미, ./는 지금의 장소를 의미*/
 
 
 const globalRouter = express.Router();/*프로젝트에 있는 모든 파일은 분리된 모듈이어서 파일 내에 있는 무언가를 바깥에 공유하기 위해서는 익스포트를 먼저 해줘야함
@@ -9,7 +9,6 @@ const globalRouter = express.Router();/*프로젝트에 있는 모든 파일은 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 export default globalRouter; /* 이런식으로 디폴트로 익스포트 할때는 내가 원하는 어떤 이름이던지 임포트 가능. 
 벗, export const를 하면 실제 이름을 그대로 써야 함*/
